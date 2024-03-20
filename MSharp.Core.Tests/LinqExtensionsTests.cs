90b26ce49c87cb7309eabd1c7a5cb23ca15345dd
Mason Adams <mason.adams@msn.com>
2024-03-19 17:00:03 -0700

    repositories, file structure changes, and some unit tests

---
+++ MSharp.Core.Tests/LinqExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using MSharp.Core.Linq;
+namespace MSharp.Core.Tests;
+public class LinqExtensionsTests
+{
+    [Fact]
+    public void Test_NonNullableInside_ReferenceType()
+    {
+        IEnumerable<string?> nullableList = new List<string?> { "Hello", null, "World" };
+        IEnumerable<string> nonNullableList = nullableList.ToNonNullableInside();
+        Assert.Equal(2, nonNullableList.Count());
+    }
+    [Fact]
+    public void Test_NonNullableInside_ValueType()
+    {
+        IEnumerable<int?> nullableList = new List<int?> { 1, null, 3 };
+        IEnumerable<int> nonNullableList = nullableList.ToNonNullableInside();
+        Assert.Equal(2, nonNullableList.Count());
+    }
+}
---
+++ MSharp.EntityFrameworkCore/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace MSharp.EntityFrameworkCore.Repositories.Interfaces;
+public interface IRepository<T> where T : class
+{
+    public T? GetSingle(Expression<Func<T, bool>> predicate);
+    public IQueryable<T> GetAll();
+    public void Add(T entity);
+    public void AddRange(IEnumerable<T> entities);
+    public void Remove(T entity);
+    public void RemoveWhere(Expression<Func<T, bool>> predicate);
+    public void SaveChanges();
+}
