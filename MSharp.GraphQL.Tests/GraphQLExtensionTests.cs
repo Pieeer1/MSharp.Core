9c59450c1cff4edcd125a6e31088d4014ced7cd5
Mason Adams <mason.adams@msn.com>
2024-04-23 13:55:29 -0700

    full decouple (#14)

---
+++ MSharp.GraphQL.Tests/GraphQLExtensionTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MSharp.Core.GraphQL.Attributes;
+using MSharp.Core.GraphQL.Extensions;
+
+namespace MSharp.GraphQL.Tests;
+public class GraphQLExtensionTests
+{
+
+    [Fact]
+    public void Test_AutomaticGraphQLInjections()
+    { 
+        IServiceCollection serviceCollection = new ServiceCollection();
+        serviceCollection.AddGraphQLServer().AddAutomaticGraphQLInjections().InitializeOnStartup();
+        //essentially a runtime check. should check if the types are added to the schema at a later time.
+    }
+
+    [Query]
+    [ExtendObjectType("Query")]
+    private class TestQuery()
+    { 
+        
+    }
+    [Mutation]
+    [ExtendObjectType("Mutation")]
+    private class TestMutation()
+    {
+        
+    }
+    [Subscription]
+    [ExtendObjectType("Subscription")]
+    private class TestSubscription()
+    {
+        
+    }
+
+}
